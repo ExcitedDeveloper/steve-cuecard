0c5a294f4434d68f3166439daea1e183c4659bb3
Steve Hunley <shunley2000@yahoo.com>
2022-05-18 10:34:12 -0500

    WIP

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -2,7 +2,7 @@ import * as React from "react"
 import { DataGrid, GridColDef, GridValueGetterParams } from "@mui/x-data-grid"
 
 const columns: GridColDef[] = [
-  { field: "id", headerName: "ID", width: 70 },
+  { field: "id", headerName: "ID", hide: true },
   { field: "firstName", headerName: "First name", width: 130 },
   { field: "lastName", headerName: "Last name", width: 130 },
   {
