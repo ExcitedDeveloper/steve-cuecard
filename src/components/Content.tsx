068326611650b6b7f517e70b7e8715d5001d8e34
Steve Hunley <shunley2000@yahoo.com>
2022-05-18 09:41:37 -0500

    Added data grid

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -2,9 +2,14 @@ import * as React from "react"
 import Container from "@mui/material/Container"
 import Typography from "@mui/material/Typography"
 import Box from "@mui/material/Box"
+import CardList from "./CardList"
 
 const Content = () => {
-  return <div style={{ backgroundColor: "teal", flex: 1 }}></div>
+  return (
+    <Container style={{ flex: 1 }}>
+      <CardList />
+    </Container>
+  )
 }
 
 export default Content
